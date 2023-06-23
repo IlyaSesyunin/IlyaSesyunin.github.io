ad6db5eef49485be86e5a7a823f3c0e5175d39df
Ilya Sesyunin <sesyunin.ilya@gmail.com>
2023-06-23 14:27:25 +0300

    Update js Credo.js

--- js.js
+++ js.js
@@ -3,5 +3,5 @@ const button = document.querySelector('button');
 
 /* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
 button.addEventListener('click', function() {
-	alert('Don t put off until tomorrow what you can do today')
+	alert("Don't put off until tomorrow what you can do today")
 })
