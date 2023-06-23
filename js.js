9fda94e1cf374da1ea43f41aeef31455d48cad7a
Ilya Sesyunin <sesyunin.ilya@gmail.com>
2023-06-23 12:07:30 +0300

    first_commit

---
+++ js.js
@@ -0,0 +1,7 @@
+/* Это объявление переменной, мы наши кнопку по тегу */
+const button = document.querySelector('button');
+
+/* Тут на кнопку навешиваем обрабочик, который ждёт клика и тогда запустит логику */
+button.addEventListener('click', function() {
+	alert('Don t put off until tomorrow what you can do today')
+})
